065e034c2f46b5efd92a7db808b19944bbad65e3
Guillaume Saint-Raymond <guillaume.saintraymond@snips.ai>
2017-04-05 15:37:33 +0200

    Macbook config

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,62 +1,47 @@
-( [ -d ${HOME}/.oh-my-zsh/custom/plugins/xw ] && git -C ${HOME}/.oh-my-zsh/custom/plugins/xw pull --rebase  > /dev/null ) || ( [ ! -d ${HOME}/.oh-my-zsh/custom/plugins/xw ] &&  git clone http://vod.canal-bis.com/stash/scm/dt/zsh-tools.git ${HOME}/.oh-my-zsh/custom/plugins/xw > /dev/null )
+alias gbpurge='git branch --merged | grep -v "\*" | grep -v "master" | grep -v "develop" | grep -v "staging" | xargs -n 1 git branch -d'
 
-alias fastssh='ssh -o PreferredAuthentications=keyboard-interactive,password'
-alias fastscp='scp -o PreferredAuthentications=keyboard-interactive,password'
+export lgg2='LGD802958690bc'
+export sgs2='00199fb36039ce'
+export nex9='HT4ALJT03255'
+export sga3='3100b9772351a27f'
 
-alias vless='vim -u /usr/share/vim/vim72/macros/less.vim'
+alias vless='vim -u /usr/share/vim/vim80/macros/less.vim'
+
+alias snipsApk='./gradlew --offline :app:installDebug'
+
+alias cleanApk='adb shell pm clear ai.snips.debug'
 
 alias lycos='grep -iIrn --color=auto'
 alias hlycos='lycos --include="*.html" --exclude-dir={bin,.target}'
 alias jlycos='lycos --include="*.java"'
 alias plycos='lycos --include="*.properties" --exclude-dir="target"'
 alias slycos='lycos --include="*.scala"'
-alias xlycos='lycos --include="*.xml" --exclude-dir="target"'
+alias xlycos='lycos --include="*.xml" --exclude-dir={target,.idea,build}'
 alias clycos='lycos --include="*.conf" --exclude-dir="target"'
 alias sbtlycos='find -name build.sbt -o -name plugins.sbt | xargs grep -in --color=auto'
 alias jsonlycos='lycos --include="*.json" --exclude-dir="target"'
+alias klycos='lycos --include="*.kt"'
+alias ymllycos='lycos --include="*.yml"'
 
 alias hfind='find . -type f -name "*.html" | grep -v bin | grep -v target | grep -i '
 alias jfind='find . -type f -name "*.java" | grep -i '
 alias pfind='find . -type f -name "*.properties" | grep -v target | grep -i '
 alias sfind='find . -type f -name "*.scala" | grep -i '
-alias xfind='find . -type f -name "*.xml" | grep -v target | grep -i '
+alias xfind='find . -type f -name "*.xml" | grep -v target | grep -v .idea | grep -i '
 alias jsonfind='find . -type f -name "*.json" | grep -v target | grep -i '
+alias kfind='find . -type f -name "*.kt" | grep -i '
 
-alias activator='~/opt/activator/activator'
+export PATH="$PATH:/home/gstraymond/Android/Sdk/platform-tools/"
+export PATH="$PATH:/home/gstraymond/Android/Sdk/tools/"
 
-alias hyperpull='find ~/git -type d -name .git | xargs -n 1 dirname | while read line; do cd $line && echo "\e[33m$line\e[0m" && git pull && git-init-scala; done; cd ~/git/xwing'
+# urxvt CTRL key
+bindkey "^[Od" backward-word  \# control-left
+bindkey "^[Oc" forward-word   \# control-righ
 
-alias clrIvy2Cache='rm -rf ~/.ivy2/cache/fr.canal'
-alias clrIvy2LocalAndCache='rm -rf ~/.ivy2/local ~/.ivy2/cache/fr.canal'
+alias refreshMon='xrandr --dpi 192 --output HDMI2 --auto --above eDP1'
+alias open='xdg-open'
 
-alias prettyJson='python -m json.tool'
+alias vi='vim'
+alias setFontSize="printf '\33]50;%s%d\007' 'xft:Consolas:pixelsize='"
 
-alias dockerHistory='history | grep "docker run" | grep -i '
-alias dlogs='docker logs --tail=1000 -f '
-
-alias dr='docker-sbt'
-
-alias gxw='~/git/xwing'
-
-alias popopop='git stash && git pull && git stash pop'
-
-alias syncHome='cp ~/.zshrc ~/git/tools/home/. && cp ~/.oh-my-zsh/custom/custom.zsh ~/git/tools/home/.oh-my-zsh/custom/. && cp ~/.gitconfig ~/git/tools/home/. && cp ~/.i3/config ~/git/tools/home/.i3/. && cp ~/.Xresources ~/git/tools/home/. && cp ~/.fonts/Input-* ~/git/tools/home/.fonts/.'
-
-bddp(){
-	local bddpCmd="http '172.30.5.17:8080$1' Access-Token:7C22881415F79684EB77F4543FC5D Accept-Language:fr-FR,fr"
-	echo $bddpCmd
-   	eval $bddpCmd
-}
-
-esAggs(){
-	local esCmd="http localhost:9200/xwing/_search size=0 aggs:='{\"$1\":{\"terms\":{\"field\":\"$1\"}}}'"
-	echo $esCmd
-   	eval $esCmd
-}
-
-esQ(){
-	local esCmd="http localhost:9200/xwing/_search\?q=$1:$2"
-	echo $esCmd
-   	eval $esCmd
-}
-
+alias syncHome='cp ~/.zshrc ~/git/tools/home/. && cp ~/.oh-my-zsh/custom/custom.zsh ~/git/tools/home/.oh-my-zsh/custom/. && cp ~/.gitconfig ~/git/tools/home/. && cp -r ~/.config/i3 ~/git/tools/home/.config/. && cp ~/.Xresources ~/git/tools/home/.'
